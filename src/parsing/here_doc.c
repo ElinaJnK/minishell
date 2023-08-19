b1e97095df618a1ea098658dabc95e60461add42
Ubuntu <karimasadykova@gmail.com>
2023-08-19 15:18:02 +0200

    fork in heredoc with signals

--- src/parsing/here_doc.c
+++ src/parsing/here_doc.c
@@ -1,30 +1,50 @@
 #include "minishell.h"
 
-static void handle_here(int sig)
+int	*exit_here(void)
 {
-	if (sig == SIGINT)
-	{
-		printf("\n\nSIGINT\n\n");
-		*exit_status() = 130;
-		exit(*exit_status());
-	}
-	else if (sig == SIGQUIT)
-	{
-		printf("\n\nQUIT\n\n");
-		ft_putstr_fd("bash: warning: here-document delimited by end-of-file\n", 2);
-		*exit_status() = 131;
-		signal(SIGQUIT, SIG_IGN);
-	}
+	static int	here = 0;
+
+	return (&here);
 }
 
-void	signal_here(void)
+static void handle_here_c(int sig)
+{
+	(void)sig;
+	//printf("\n\nSIGINT\n\n");
+	close(0);
+	*exit_here() = 130;
+
+	// if (*pid_child() != -1)
+	// 	kill(*pid_child(), SIGINT);
+}
+
+static void handle_here_d(int sig)
+{
+	(void)sig;
+	printf("\n\nQUIT\n\n");
+	close(0);
+	//ft_putstr_fd("bash: warning: here-document delimited by end-of-file\n", 2);
+	*exit_here() = 131;
+	//signal(SIGQUIT, SIG_IGN);
+}
+
+void	signal_here_c(void)
 {
 	struct sigaction	act;
 
-	act.sa_handler = handle_here;
+	act.sa_handler = handle_here_c;
 	sigemptyset(&act.sa_mask);
 	act.sa_flags = 0;
 	sigaction(SIGINT, &act, NULL);
+}
+
+void	signal_here_d(void)
+{
+	struct sigaction	act;
+
+	act.sa_handler = handle_here_d;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
 	sigaction(SIGQUIT, &act, NULL);
 }
 
@@ -65,14 +85,18 @@ int	read_stdin(int fd, char *limiter, int type, t_env *env)
 {
 	char	*line;
 
+	signal_here_c();
+	signal_here_d();
 	ft_putstr_fd("> ", 1);
 	line = get_next_line(0);
+	if (!line && *exit_here() == 131)  // EOF detected
+	{
+		ft_putstr_fd("bash: warning: here-document delimited by end-of-file\n", 2);
+		return (*exit_here()); 
+	}
 	while (line && ft_strncmp(line, limiter, ft_max(ft_strlen(line) - 1,
 				ft_strlen(limiter))) != 0)
 	{
-		signal_here();
-		if (*exit_status() == 130 || *exit_status() == 131)
-			{printf("\n\nHERE\n\n"); break ;}
 		if (type == DREDIR2)
 		{
 			line = here_doc_expand(line, env);
@@ -84,19 +108,40 @@ int	read_stdin(int fd, char *limiter, int type, t_env *env)
 		free(line);
 		ft_putstr_fd("> ", 1);
 		line = get_next_line(0);
+		if (*exit_here() == 131)  // EOF detected
+		{
+			ft_putstr_fd("bash: warning: here-document delimited by end-of-file\n", 2);
+			return (*exit_here()); 
+		}
+	}
+	if (*exit_here() == 130)
+		return (*exit_here());
+	if (*exit_here() == 131)  // EOF detected
+	{
+		ft_putstr_fd("bash: warning: here-document delimited by end-of-file\n", 2);
+		return (*exit_here()); 
 	}
 	if (line)
 		free(line);
 	return (close(fd), EXIT_SUCCESS);
 }
 
+// static void	handle_sigint_child(int sig)
+// {
+// 	(void)sig;
+// 	*exit_status() = 130;
+// 	exit(*exit_status());
+// }
+
 int	open_here_doc(int *pipe_fds, char *limiter, int type, t_env *env)
 {
 	pid_t	pid;
 	int		status;
+	int		ex;
 
 	pid = fork();
 	status = 0;
+	ex = 0;
 	if (pid == -1)
 	{
 		perror("fork");
@@ -105,13 +150,21 @@ int	open_here_doc(int *pipe_fds, char *limiter, int type, t_env *env)
 	if (pid == 0)
 	{
 		sig_child();
-		read_stdin(pipe_fds[1], limiter, type, env);
-		exit(*exit_status());
+		//signal(SIGINT, handle_sigint_child);
+		ex = read_stdin(pipe_fds[1], limiter, type, env);
+		exit(ex);
 	}
 	else
 	{
+		//printf("\n\nLOLO\n\n");
 		waitpid(pid, &status, 0);
-		*exit_status() = status;
+		if (WIFEXITED(status))
+			*exit_status() = WEXITSTATUS(status);
+		else if (WIFSIGNALED(status))
+			*exit_status() = WTERMSIG(status) + 128;
+		if (*exit_status() == EXIT_FAILURE)
+			return (EXIT_FAILURE);
+		//printf("\n\nLOLO2\n\n");
 	}
 	return (EXIT_SUCCESS);
 }
